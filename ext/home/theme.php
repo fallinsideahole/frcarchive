8a15efc55963a3b1ceb25307f4cbd952328dbf58
fallinsideahole <160703170+fallinsideahole@users.noreply.github.com>
2024-02-21 01:01:58 -0500

    added text on index

--- ext/home/theme.php
+++ ext/home/theme.php
@@ -29,17 +29,17 @@ EOD
         );
     }
 
-    public function build_body(string $sitename, string $main_links, string $main_text, string $contact_link, string $num_comma, string $counter_text): string
+    public function build_body(string $sitename, string $main_links, string $main_text, string $contact_link, $num_comma, string $counter_text): string
     {
         $main_links_html = empty($main_links) ? "" : "<div class='space' id='links'>$main_links</div>";
-        $message_html = empty($main_text) ? "" : "<div class='space' id='message'>$main_text</div>";
-        $counter_html = empty($counter_text) ? "" : "<div class='space' id='counter'>$counter_text</div>";
+        $message_html = empty($main_text)     ? "" : "<div class='space' id='message'>$main_text</div>";
+        $counter_html = empty($counter_text)  ? "" : "<div class='space' id='counter'>$counter_text</div>";
         $contact_link = empty($contact_link) ? "" : "<br><a href='$contact_link'>Contact</a> &ndash;";
         $search_html = "
 			<div class='space' id='search'>
-				<form action='".search_link()."' method='GET'>
-				<input name='search' size='30' type='search' value='' class='autocomplete_tags' autofocus='autofocus' />
-				<input type='hidden' name='q' value='post/list'>
+				<form action='".make_link("post/list")."' method='GET'>
+				<input name='search' size='30' type='search' value='' class='autocomplete_tags' autofocus='autofocus' autocomplete='off' />
+				<input type='hidden' name='q' value='/post/list'>
 				<input type='submit' value='Search'/>
 				</form>
 			</div>
@@ -53,9 +53,11 @@ EOD
 			$counter_html
 			<div class='space' id='foot'>
 				<small><small>
-				$contact_link" . (empty($num_comma) ? "" : " Serving $num_comma posts &ndash;") . "
+				$contact_link Serving $num_comma posts &ndash;
 				Running <a href='https://code.shishnet.org/shimmie2/'>Shimmie2</a>
 				</small></small>
+				</br>
+				This project is not affiliated with or endorsed by <i>FIRST</i> or any of its partners.
 			</div>
 		</div>";
     }
